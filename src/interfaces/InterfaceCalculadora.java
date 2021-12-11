2cd112750b531e5b248d13c08ef589a4f712e733
Mapie Victorino Mamunhe <mmamunhe@gmail.com>
2021-12-11 23:27:23 +0200

    Assinatura dos metodos
    Adicionado um comentário descritivo da função de cada método para a classe CalculadoraSimples
    Reescrita da assinatura dos metodos genéricos afim de serem mais génericos

--- src/interfaces/InterfaceCalculadora.java
+++ src/interfaces/InterfaceCalculadora.java
@@ -2,27 +2,27 @@ package interfaces;
 
 public interface InterfaceCalculadora {
 	//Operacoes de soma
-	public <E> double somar(E a, E b);
+	public <E extends Number> E somar(E a, E b);
 	public double somar(double a, double b);
 	public double somar(double arr[]);
-	public <E> double somar(E arr[]);
+	public <E extends Number> E somar(E arr[]);
 	
 	//Operacoes de subtração
-	public <E> double subtrair(E a, E b);
+	public <E extends Number> E subtrair(E a, E b);
 	public double subtrair(double a, double b);
 	public double subtrair(double arr[]);
-	public <E> double subtrair(E arr[]);
+	public <E extends Number>  E subtrair(E arr[]);
 	
 	//Operacoes de multiplicacao
-	public <E> double multiplicacao(E a, E b);
+	public <E extends Number> E multiplicacao(E a, E b);
 	public double multiplicacao(double a, double b);
 	public double multiplicacao(double arr[]);
-	public <E> double multiplicacao(E arr[]);
+	public <E extends Number>  E multiplicacao(E arr[]);
 	
 	//Operacoes de divisao
-	public <E> double divisao(E a, E b);
+	public <E extends Number> E divisao(E a, E b);
 	public double divisao(double a, double b);
 	public double divisao(double arr[]);
-	public <E> double divisao(E arr[]);
+	public <E extends Number> E divisao(E arr[]);
 	
 }
--- src/miniprogramas/classes/CalculadoraSimples.java
+++ src/miniprogramas/classes/CalculadoraSimples.java
@@ -4,100 +4,156 @@ import interfaces.InterfaceCalculadora;
 
 public class CalculadoraSimples implements InterfaceCalculadora {
 
+	/*
+	 * Esse metodo somar b ao valor a e retornar o resultado
+	 * */
 	@Override
-	public <E> double somar(E a, E b) {
-		// TODO Auto-generated method stub
-		return 0;
+	public <E extends Number> E somar(E a, E b) {
+		E resultado = a;
+		
+		return resultado;
 	}
 
+	/*
+	 * Esse metodo somar b ao valor a e retornar o resultado
+	 * */
 	@Override
 	public double somar(double a, double b) {
-		// TODO Auto-generated method stub
-		return 0;
+		return a+b;
 	}
 
+	/*
+	 * Esse metodo somar b ao valor a e retornar o resultado
+	 * */
 	@Override
 	public double somar(double[] arr) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/*
+	 * Esse metodo somar b ao valor a e retornar o resultado
+	 * */
 	@Override
-	public <E> double somar(E[] arr) {
-		// TODO Auto-generated method stub
-		return 0;
+	public <E extends Number> E somar(E[] arr) {
+		Number resultado = arr[0];
+		
+		return (E)resultado;
+	}
+	
+	/*
+	 * Esse metodo subtrai b do valor a e retornar o resultado
+	 * */
+	@Override
+	public <E extends Number> E subtrair(E a, E b) {
+		E resultado = a;
+		
+		return resultado;
 	}
 
 	@Override
-	public <E> double subtrair(E a, E b) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
+	/*
+	 * Esse metodo subtrai b do valor a e retornar o resultado
+	 * */
 	public double subtrair(double a, double b) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return a-b;
 	}
 
+	/*
+	 * Esse metodo subtrai b do valor a e retornar o resultado
+	 * */
 	@Override
 	public double subtrair(double[] arr) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/*
+	 * Esse metodo subtrai b do valor a e retornar o resultado
+	 * */
 	@Override
-	public <E> double subtrair(E[] arr) {
-		// TODO Auto-generated method stub
-		return 0;
+	public <E extends Number> E subtrair(E[] arr) {
+		Number resultado = arr[0];
+		
+		return (E)resultado;
 	}
 
+	/*
+	 * Esse metodo multiplica a com b e retornar o resultado
+	 * */
 	@Override
-	public <E> double multiplicacao(E a, E b) {
-		// TODO Auto-generated method stub
-		return 0;
+	public <E extends Number> E multiplicacao(E a, E b) {
+		E resultado = a;
+		
+		return resultado;
 	}
 
+	/*
+	 * Esse metodo multiplica a com b e retornar o resultado
+	 * */
 	@Override
 	public double multiplicacao(double a, double b) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/*
+	 * Esse metodo multiplica a com b e retornar o resultado
+	 * */
 	@Override
 	public double multiplicacao(double[] arr) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	
 
+	/*
+	 * Esse metodo multiplica a com b e retornar o resultado
+	 * */
 	@Override
-	public <E> double multiplicacao(E[] arr) {
-		// TODO Auto-generated method stub
-		return 0;
+	public <E extends Number> E multiplicacao(E[] arr) {
+		Number resultado = arr[0];
+		
+		return (E)resultado;
 	}
 
+	/*
+	 * Esse metodo divide a por b e retornar o resultado
+	 * */
 	@Override
-	public <E> double divisao(E a, E b) {
-		// TODO Auto-generated method stub
-		return 0;
+	public <E extends Number> E divisao(E a, E b) {
+		E resultado = a;
+		
+		return resultado;
 	}
 
+	/*
+	 * Esse metodo divide a por b e retornar o resultado
+	 * */
 	@Override
 	public double divisao(double a, double b) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/*
+	 * Esse metodo divide a por b e retornar o resultado
+	 * */
 	@Override
 	public double divisao(double[] arr) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/*
+	 * Esse metodo divide a por b e retornar o resultado
+	 * */
 	@Override
-	public <E> double divisao(E[] arr) {
-		// TODO Auto-generated method stub
-		return 0;
+	public <E extends Number> E divisao(E[] arr) {
+		Number resultado = arr[0];
+				
+		return (E)resultado;
 	}
 
 }
